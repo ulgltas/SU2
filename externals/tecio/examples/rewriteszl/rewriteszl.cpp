f7df85125c2ae5ddc2f4aeb0d4f89522e8174c49
Martin-Lacroix <47146335+Martin-Lacroix@users.noreply.github.com>
2024-07-18 15:19:12 +0200

    Ubuntu 24 Compat

--- externals/tecio/examples/rewriteszl/rewriteszl.cpp
+++ externals/tecio/examples/rewriteszl/rewriteszl.cpp
@@ -12,6 +12,7 @@
 #include <string>
 #include <stdexcept>
 #include <vector>
+#include <cstdint>
 
 int32_t countFaceConnections(int32_t const* faceConnections, int64_t numFaceValues, int32_t mode);
 
